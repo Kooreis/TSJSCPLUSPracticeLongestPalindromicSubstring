fae5ae2845157fa7a7db9f2abfc43fd469d3edff
Kooreis <kooreis.official@gmail.com>
2023-04-27 18:51:19 -0500

    In the second update, the function 'longestPalindromicSubstring' is defined. This function takes a string as input and returns the longest palindromic substring. It uses the 'expandAroundCenter' function to find palindromes.

--- solution.cpp
+++ solution.cpp
@@ -1,2 +1,16 @@
-#include <iostream>
-#include <string>
+std::string longestPalindromicSubstring(std::string s) {
+    int n = s.size();
+    if (n == 0) return "";
+    std::string longest = s.substr(0, 1);
+    for (int i = 0; i < n; i++) {
+        std::string temp = expandAroundCenter(s, i, i);
+        if (temp.size() > longest.size()) {
+            longest = temp;
+        }
+        temp = expandAroundCenter(s, i, i + 1);
+        if (temp.size() > longest.size()) {
+            longest = temp;
+        }
+    }
+    return longest;
+}
