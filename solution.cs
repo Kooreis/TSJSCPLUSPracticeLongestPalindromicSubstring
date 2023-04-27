268bb2c249e70bbbe4eee168afc75cabd598206a
Kooreis <kooreis.official@gmail.com>
2023-04-27 18:51:19 -0500

    This is a helper function that expands around the center (left and right) and checks if the characters are the same. If they are, it continues expanding until it finds a pair of characters that are not the same. It returns the length of the palindrome.

--- solution.cs
+++ solution.cs
@@ -1,14 +1,8 @@
-function longestPalindromicSubstring(string) {
-    let start = 0;
-    let end = 0;
-    for (let i = 0; i < string.length; i++) {
-        let len1 = expandAroundCenter(string, i, i);
-        let len2 = expandAroundCenter(string, i, i + 1);
-        let len = Math.max(len1, len2);
-        if (len > end - start) {
-            start = i - Math.floor((len - 1) / 2);
-            end = i + Math.floor(len / 2);
-        }
+function expandAroundCenter(string, left, right) {
+    let L = left, R = right;
+    while (L >= 0 && R < string.length && string.charAt(L) === string.charAt(R)) {
+        L--;
+        R++;
     }
-    return string.substring(start, end + 1);
+    return R - L - 1;
 }
