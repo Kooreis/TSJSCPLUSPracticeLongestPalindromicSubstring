02351380cd4c355f965111bc584222f04c08a341
Kooreis <kooreis.official@gmail.com>
2023-04-27 18:51:19 -0500

    This update adds a loop that iterates over the length of the input string. For each character, it calls the 'expandAroundCenter' function twice, once for the character itself and once for the character and its next neighbor. It then calculates the maximum length of the two results. If this length is greater than the difference between 'end' and 'start', it updates 'start' and 'end' to represent the new palindrome.

--- solution.ts
+++ solution.ts
@@ -1,4 +1,9 @@
-function longestPalindromicSubstring(s: string): string {
-    if (s.length < 1 || s == null) return "";
-    let start = 0, end = 0;
-}
+for (let i = 0; i < s.length; i++) {
+        let len1 = expandAroundCenter(s, i, i);
+        let len2 = expandAroundCenter(s, i, i + 1);
+        let len = Math.max(len1, len2);
+        if (len > end - start) {
+            start = i - Math.floor((len - 1) / 2);
+            end = i + Math.floor(len / 2);
+        }
+    }
